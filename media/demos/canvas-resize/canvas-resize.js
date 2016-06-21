6b38f5a0d553e8eb3f3352fb53f9bfa63943ba30
Ionut Colceriu <contact@ghinda.net>
2016-06-21 12:53:25 +0300

    More tweaks for the load-order fix.

--- media/demos/canvas-resize/canvas-resize.js
+++ media/demos/canvas-resize/canvas-resize.js
@@ -5,33 +5,33 @@ var $direct = document.querySelector('#direct-resize')
 var $step = document.querySelector('#step-resize')
 var $img = document.querySelector('img')
 
+function downstep (image, size, callback) {
+    var scale = 2
+    var img = new Image()
+    img.src = image.src
+
+    img.addEventListener('load', function () {
+      while (img.width / scale > size) {
+        var c = document.createElement('canvas')
+        var ctx = c.getContext('2d')
+
+        img.width = img.width / scale
+        img.height = img.height / scale
+
+        c.width = img.width
+        c.height = img.height
+
+        ctx.drawImage(img, 0, 0, c.width, c.height)
+
+        img.src = c.toDataURL('image/png')
+      }
+
+      callback(img)
+    })
+}
+
 // jotted sometimes loads the js before the html
 if ($img) {
-
-  function downstep (image, size, callback) {
-      var scale = 2
-      var img = new Image()
-      img.src = image.src
-
-      img.addEventListener('load', function () {
-        while (img.width / scale > size) {
-          var c = document.createElement('canvas')
-          var ctx = c.getContext('2d')
-
-          img.width = img.width / scale
-          img.height = img.height / scale
-
-          c.width = img.width
-          c.height = img.height
-
-          ctx.drawImage(img, 0, 0, c.width, c.height)
-
-          img.src = c.toDataURL('image/png')
-        }
-
-        callback(img)
-      })
-  }
 
   $img.addEventListener('load', function () {
     // direct canvas resize
