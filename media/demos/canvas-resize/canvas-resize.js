664b750494785bd7177f0acf22276a31bfdc8eb5
Ionut Colceriu <contact@ghinda.net>
2016-06-21 12:52:58 +0300

    Refactor the load-order fix.

--- media/demos/canvas-resize/canvas-resize.js
+++ media/demos/canvas-resize/canvas-resize.js
@@ -6,41 +6,41 @@ var $step = document.querySelector('#step-resize')
 var $img = document.querySelector('img')
 
 // jotted sometimes loads the js before the html
-if (!$img) {
-  return
-}
+if ($img) {
 
-function downstep (image, size, callback) {
-    var scale = 2
-    var img = new Image()
-    img.src = image.src
+  function downstep (image, size, callback) {
+      var scale = 2
+      var img = new Image()
+      img.src = image.src
 
-    img.addEventListener('load', function () {
-      while (img.width / scale > size) {
-        var c = document.createElement('canvas')
-        var ctx = c.getContext('2d')
+      img.addEventListener('load', function () {
+        while (img.width / scale > size) {
+          var c = document.createElement('canvas')
+          var ctx = c.getContext('2d')
 
-        img.width = img.width / scale
-        img.height = img.height / scale
+          img.width = img.width / scale
+          img.height = img.height / scale
 
-        c.width = img.width
-        c.height = img.height
+          c.width = img.width
+          c.height = img.height
 
-        ctx.drawImage(img, 0, 0, c.width, c.height)
+          ctx.drawImage(img, 0, 0, c.width, c.height)
 
-        img.src = c.toDataURL('image/png')
-      }
+          img.src = c.toDataURL('image/png')
+        }
 
-      callback(img)
+        callback(img)
+      })
+  }
+
+  $img.addEventListener('load', function () {
+    // direct canvas resize
+    $direct.getContext('2d').drawImage($img, 0, 0, 200, 133)
+
+    // down-step canvas resize
+    downstep($img, 200, function (img) {
+      $step.getContext('2d').drawImage(img, 0, 0, 200, 133)
     })
-}
-
-$img.addEventListener('load', function () {
-  // direct canvas resize
-  $direct.getContext('2d').drawImage($img, 0, 0, 200, 133)
-
-  // down-step canvas resize
-  downstep($img, 200, function (img) {
-    $step.getContext('2d').drawImage(img, 0, 0, 200, 133)
   })
-})
+
+}
